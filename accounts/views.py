eeb7e576a6f61e2d72acf76c1bf90a5e44801b40
mike <mpgarate@gmail.com>
2014-12-11 22:16:47 -0500

    hacky version of user registration working

--- accounts/views.py
+++ accounts/views.py
@@ -7,7 +7,12 @@ def register(request):
         form = UserCreationForm(request.POST)
         if form.is_valid():
             new_user = form.save()
-            authenticate()
+            print(request.POST)
+            username = request.POST['username']
+            password = request.POST['password1']
+
+            new_user = authenticate(username=username, password=password)
+
             login(request, new_user)
             return redirect("index")
         else:
