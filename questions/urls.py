0be2573cc34bfca005ed26b51f6e255e5148ef2c
mike <mpgarate@gmail.com>
2014-12-15 20:02:55 -0500

    remove rss feature

--- questions/urls.py
+++ questions/urls.py
@@ -29,9 +29,5 @@ urlpatterns = patterns('',
     url(r'^vote_down_answer/(?P<answer_id>\d+)/$', views.vote_down_answer,
         name='vote_down_answer'),
 
-    # rss
-    url(r'^feed/$', feeds.LatestQuestionsFeed(), name="feed"),
-    url(r'^(?P<question_id>\d+)/answers/feed/$', feeds.LatestAnswersFeed(),
-        name="answers_feed"),
 )
 
