04696d351b3b6aa921f6782dc2e30c5fb30211e6
mike <mpgarate@gmail.com>
2014-12-15 19:56:44 -0500

    add rss feature

--- questions/models.py
+++ questions/models.py
@@ -4,6 +4,7 @@ from django.contrib.auth.models import User
 
 from taggit.managers import TaggableManager
 
+from django.contrib.syndication.views import Feed
 
 import datetime
 
@@ -69,3 +70,35 @@ class AnswerVote(models.Model):
     answer = models.ForeignKey(Answer)
     value = models.IntegerField()
 
+class LatestAnswersFeed(Feed):
+    link = "/"
+
+    def get_object(self, request, question_id):
+        return get_object_or_404(Question, pk=question_id)
+
+    def items(self, question):
+        return Answer.objects.filter(question=question).order_by('-date')
+
+    def title(self, item):
+        return "Latest answers on Fauxra for the question: %s" % item.question.text
+
+    def item_title(self, item):
+        return item.text
+
+    def item_link(self, item):
+        return reverse("questions:show", args=[item.question.id])
+
+
+class LatestQuestionsFeed(Feed):
+    title = "Latest questions from Fauxra"
+    link = "/"
+    description = "See the latest questions posted to Fauxra."
+
+    def items(self):
+        return Question.objects.order_by('-date')
+
+    def item_title(self, item):
+        return truncatechars(item.text, 500)
+
+    def item_link(self, item):
+        return reverse("questions:show", args=[item.id])
